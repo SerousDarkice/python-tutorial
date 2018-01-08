283af93c0f0638028e5b7b657f6a189aa593493f
Eddie Jennings, Jr <eddie@eddiejennings.net>
2018-01-07 21:56:32 -0500

    Practice using round() function

---
+++ ex5drill3.py
@@ -0,0 +1,17 @@
+print("Testing out rounding.")
+
+number1 = 50.0
+number2 = 3.0
+
+number3 = number1 / number2
+
+print("Here is number 3: ", round(number3))
+print("Here is number 3 without rounding: ", number1 / number2)
+
+number4 = 10.0
+number5 = 3.0
+
+number6 = round(number4 / number5)
+
+print("here is number 6: ", number6)
+print("Here is number 6 without rounding: ", number4 / number5)

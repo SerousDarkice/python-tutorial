c8bd5cd2b02e17ab33d6272b234b9f2e5fb8eab4
Eddie Jennings, Jr <eddie@eddiejennings.net>
2018-01-07 21:46:02 -0500

    Complete Exercise 5

---
+++ ex5drill2.py
@@ -0,0 +1,8 @@
+inches = 36
+centimeters = inches * 2.54
+
+fahrenheit = 0
+celsius = (fahrenheit - 32) * (5 / 9)
+
+print(f"{inches} inches converts to {centimeters} centimeters.")
+print(f"{fahrenheit} degrees coverts to {celsius} degrees Celsius.")

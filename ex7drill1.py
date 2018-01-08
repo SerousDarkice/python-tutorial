9e7249d9b60c8c1e8738964138e7669e1b13e2a4
Eddie Jennings, Jr <eddie@eddiejennings.net>
2018-01-07 23:00:44 -0500

    Complete exercise 7

---
+++ ex7drill1.py
@@ -0,0 +1,22 @@
+print("Mary Had a little lamb.") # print a string
+print("Its fleece was white as {}.".format('snow')) # print a string and format it with the string snow
+print("And everywhere that Mary went.") # print a string
+print("." * 10) # print 10 strings
+
+end1 = "C" # assign a string to a variable
+end2 = "h" # assign a string to a variable
+end3 = "e" # assign a string to a variable
+end4 = "e" # assign a string to a variable
+end5 = "s" # assign a string to a variable
+end6 = "e" # assign a string to a variable
+end7 = "B" # assign a string to a variable
+end8 = "u" # assign a string to a variable
+end9 = "r" # assign a string to a variable
+end10 = "g" # assign a string to a variable
+end11 = "e" # assign a string to a variable
+end12 = "r" # assign a string to a variable
+
+# watch that comma at the end.  try removing it to see what happens
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ') # print variables, but do not do a line return, rather print a space
+print(end7 + end8 + end9 + end10 + end11 + end12) # print variables
